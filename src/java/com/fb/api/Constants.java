18306a501d9d3bca98c3d4a27959f399d8d0b932
ShanthalakshmiM <shantha.aauma@gmail.com>
2017-12-21 11:43:49 +0530

    Update Constants.java

--- src/java/com/fb/api/Constants.java
+++ src/java/com/fb/api/Constants.java
@@ -10,11 +10,10 @@ package com.fb.api;
  * @author HP
  */
 public class Constants {
-    public static final String MY_ACCESS_TOKEN = "EAACEdEose0cBAFAfnKPPMQzzfQ4N4bNmcc1NdTcUqINZCmvNsZABjaxrHS1Fe5AkmLsgu2PkfRa0IVQH95rxxkmvR58ZCNdyofTyMLIwanRm35YoTD5ZCSk8ymcj95roqjBedvdZCgTA0Qet0OHLsRIDXW64a87cKGZCukzWZA1pUCmR2ZA8z0x4eLDewYttLTfmAGgY7LZBI5AZDZD";
-    public static final String APP_ID = "1248949948540245";
-    public static final String APP_SECRET = "96977b0709a53e4d06a8633a3efe03a5";
-    public static final String PAGE_ACCESS_TOKEN = "EAARv6dIGZAVUBAAUvxNqUjlkBznZAYyzeT7fqZBDhpQlOZCRO4M8mFeYZAA76odPFtwUXQAjANqfZAwPQ9BVWJNpsZB44jHPRCpiF9BEh2nnZC44ZATCfIwhIUCrhoRgKzaRPrOwSI7Y896TIBsKgyZALAKqbADpEPwZA9pQzIqSnz7vYtEfS4MZAV76";
+    public static final String MY_ACCESS_TOKEN = "XXXXXXXX";
+    public static final String APP_SECRET = "XXXXXX";
+    public static final String PAGE_ACCESS_TOKEN = "XXXXXX";
     public static final String REDIRECT_URI = "http://localhost:8080/Project/Info";
-    public static final String PAGE_ID = "200864727141137";
+    public static final String PAGE_ID = "XXXXXX";
     public static String post_id = null;
 }
